41ab2c0cb54befd55ac112da4d90bb8bb445c194
Jan Dostal <jan.dostal@tul.cz>
2023-09-29 15:18:17 +0200

    Completed forth exercise

--- Exercise_04/Program.cs
+++ Exercise_04/Program.cs
@@ -32,20 +32,67 @@ namespace Exercise_03
                 }
             }
 
-            IList<BigInteger> candidates;
+            IList<BigInteger> palindromicNumberCandidates;
+            IList<BigInteger> inputNumberCandidatePrefixes;
 
             NumberFormatInfo bigIntegerFormatter = new NumberFormatInfo();
             int inputNumberDigitsLength;
 
-            int middleElementIndex;
+            int inputNumberMiddleElementPositionOrder;
+            BigInteger inputNumberPrefix;
+            char[] inputNumberCandidateStringPrefix;
+            string palindromicNumberConstructedCandidate;
 
-            foreach (var num in inputNumbers.AsEnumerable())
+
+            BigInteger minDifferenceToClosestPalindromicNumber;
+            BigInteger closestPalindromicNumber;
+
+            foreach (var number in inputNumbers.AsEnumerable())
             {
-                candidates = new List<BigInteger>();
+                palindromicNumberCandidates = new List<BigInteger>();
 
-                inputNumberDigitsLength = num.ToString("D", bigIntegerFormatter).Length;
+                inputNumberDigitsLength = number.ToString("D", bigIntegerFormatter).Length;
 
-                middleElementIndex = 
+                if (number < 9)
+                {
+                    Console.WriteLine(number + 1);
+                    continue;
+                }
+
+                inputNumberMiddleElementPositionOrder = (inputNumberDigitsLength + 1) / 2;
+
+                palindromicNumberCandidates.Add(BigInteger.Pow(10, inputNumberDigitsLength) + 1);
+
+                inputNumberPrefix = BigInteger.Parse(number.ToString("D", bigIntegerFormatter).Substring(0, inputNumberMiddleElementPositionOrder));
+
+                inputNumberCandidatePrefixes = new List<BigInteger>() { inputNumberPrefix, inputNumberPrefix + 1 };
+
+                foreach (var prefix in inputNumberCandidatePrefixes.AsEnumerable())
+                {
+                    inputNumberCandidateStringPrefix = prefix.ToString("D", bigIntegerFormatter).ToCharArray();
+
+                    if (inputNumberDigitsLength % 2 != 0) inputNumberCandidateStringPrefix = inputNumberCandidateStringPrefix.SkipLast(1).Cast<char>().ToArray();
+
+                    Array.Reverse(inputNumberCandidateStringPrefix);
+
+                    palindromicNumberConstructedCandidate = prefix + new string(inputNumberCandidateStringPrefix);
+
+                    palindromicNumberCandidates.Add(BigInteger.Parse(palindromicNumberConstructedCandidate));
+                }
+
+                minDifferenceToClosestPalindromicNumber = BigInteger.Parse($"{decimal.MaxValue}");
+                closestPalindromicNumber = number;
+
+                foreach (var candidate in palindromicNumberCandidates.AsEnumerable())
+                {
+                    if (candidate > number && minDifferenceToClosestPalindromicNumber > candidate - number)
+                    {
+                        closestPalindromicNumber = candidate;
+                        minDifferenceToClosestPalindromicNumber = candidate - number;
+                    }
+                }
+
+                Console.WriteLine(closestPalindromicNumber);
             }
         }
     }
